ff158c16da2cbc0d4f7aadd6544ec6ae149cd246
Léo <cdf.leo.merkel@gmail.com>
2026-06-18 21:30:06 +0200

    finishing interfaces & pnpm format

---
+++ README.md
@@ -0,0 +1,1 @@
+# LPDWCA-Chat
--- vite.config.js
+++ vite.config.js
@@ -5,30 +5,30 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [
-        VitePWA({
-            registerType: 'autoUpdate',
-            devOptions: {
-                enabled: true
-            },
-            manifest: {
-                name: 'LPDWCA-Chat',
-                short_name: 'LP-Chat',
-                description: 'Tchat de la LPDWCA',
-                theme_color: '#ffffff',
-                icons: [
-                    {
-                        src: 'javascript.svg',
-                        sizes: '32x32',
-                        type: 'image/svg'
-                    }
-                ]
-            }
-        }),
-    ],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        },
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: 'LPDWCA-Chat',
+        short_name: 'LP-Chat',
+        description: 'Tchat de la LPDWCA',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'javascript.svg',
+            sizes: '32x32',
+            type: 'image/svg',
+          },
+        ],
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
 })
